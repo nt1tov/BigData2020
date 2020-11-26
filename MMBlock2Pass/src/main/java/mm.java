951ed509c0b02bfea716ed3f2562935bbf406042
nt1tov <1nikolay.titov@gmail.com>
2020-11-26 17:48:07 +0300

    upd: 1st mapper works well

--- MMBlock2Pass/src/main/java/mm.java
+++ MMBlock2Pass/src/main/java/mm.java
@@ -151,9 +151,9 @@ public class mm {
 
     }
 
-    public static class BlockMapper extends Mapper<Object, Text, MatrixBlockKey, MatrixBlockValue> {
+    public static class MultBlockMapper extends Mapper<Object, Text, MatrixBlockKey, MatrixBlockValue> {
 
-        public static final Log log = LogFactory.getLog(BlockMapper.class);
+        public static final Log log = LogFactory.getLog(MultBlockMapper.class);
 
         @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
@@ -164,25 +164,34 @@ public class mm {
             long N_SIZE = conf.getLong("n_size", -1);
             long P_SIZE = conf.getLong("p_size", -1);
 
-            assert (M_SIZE != -1 && N_SIZE != -1 && P_SIZE != -1);
+            log.info("block_size=" + BLOCK_SIZE + " M_SIZE" + M_SIZE + " N_SIZE=" + N_SIZE + " P_size=" + P_SIZE);
 
-            long LeftBlockColSize = M_SIZE / BLOCK_SIZE;
-            long RightBlockRowSize = P_SIZE / BLOCK_SIZE;
+            if( M_SIZE == -1 || N_SIZE == -1 || P_SIZE == -1){
+                throw new IOException();
+            }
+
             String[] input_record = value.toString().split("\t");
+//            log.info(input_record[0]);
+//            log.info(input_record[1]);
+//            log.info(input_record[2]);
+//            log.info(input_record[3]);
+
             String MatrixTag = input_record[0];
             long col =  Long.parseLong(input_record[1]);
             long row =  Long.parseLong(input_record[2]);
             double val = Double.parseDouble(input_record[3]);
 
-            log.info(conf.get("mm.tags"));
-            String LeftMatrixTag = "A";
-            String RightMatrixTag = "B";
 
+            String LeftMatrixTag = conf.get("mm.tags").substring(0, 1);
+            String RightMatrixTag = conf.get("mm.tags").substring(1, 2);;
+//            log.info(LeftMatrixTag);
+//            log.info(RightMatrixTag);
             MatrixBlockKey outKey;
             MatrixBlockValue outVal;
 
             if(MatrixTag.equals(LeftMatrixTag)){
                 long blockColumns = P_SIZE / BLOCK_SIZE;
+                log.info("blockColumns="+ blockColumns);
                 for (int i = 0; i < blockColumns; ++i){
                     context.write(new MatrixBlockKey(row / BLOCK_SIZE, col / BLOCK_SIZE, i),
                                 new MatrixBlockValue(MatrixTag, row % BLOCK_SIZE,
@@ -191,6 +200,7 @@ public class mm {
             }
             else if(MatrixTag.equals(RightMatrixTag)){
                 long blockRows = M_SIZE / BLOCK_SIZE;
+                log.info("blockRows="+ blockRows);
                 for (int i = 0; i < blockRows; ++i){
                     context.write(new MatrixBlockKey(i, row / BLOCK_SIZE, col / BLOCK_SIZE),
                             new MatrixBlockValue(MatrixTag, row % BLOCK_SIZE,
@@ -207,8 +217,8 @@ public class mm {
 
 
 
-    public static class BlockReducer extends Reducer<Text, Text, Text, Text> {
-        public static final Log log = LogFactory.getLog(BlockReducer.class);
+    public static class MultBlockReducer extends Reducer<Text, Text, Text, Text> {
+        public static final Log log = LogFactory.getLog(MultBlockReducer.class);
 
         @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
@@ -224,6 +234,7 @@ public class mm {
 
 
     public static void main(String[] args) throws Exception {
+        final Log log = LogFactory.getLog(mm.class);
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
 
@@ -233,41 +244,52 @@ public class mm {
         }
         conf.setIfUnset("mm.tags", "ABC");
         conf.setIfUnset("mm.float-format", "%.3f");
-        conf.setIfUnset("mm.reduce.tasks", "1");
+        conf.setIfUnset("mapred.reduce.tasks", "1");
+        conf.setIfUnset("mm.groups", "1");
         String tags = conf.get("mm.tags");
 
         assert (tags.length() == 3);
 
         //parse matrix A size
         BufferedReader reader;
-        Long m = -1L, n = -1L, p = -1L, o = -1L;
+        long m = -1L, n = -1L, p = -1L, o = -1L, n1 = -1L;
         try {
             reader = new BufferedReader(new FileReader(otherArgs[0] + "/size"));
-            String[] size_str = reader.readLine().split("\t");
+            String input_line = reader.readLine();
+            String[] size_str = input_line.split("\t");
+            log.info(input_line);
             m = Long.parseLong(size_str[0]);
             n = Long.parseLong(size_str[1]);
+            log.info(size_str);
             reader.close();
         } catch (IOException e) {
+            System.out.println("exception Left");
             e.printStackTrace();
+            System.exit(1);
         }
         //parse matrix B size
         try {
             reader = new BufferedReader(new FileReader(otherArgs[1] + "/size"));
-            String[] size_str = reader.readLine().split("\t");
-            p =  Long.parseLong(size_str[0]);
-            o =  Long.parseLong(size_str[1]);
+            String input_line = reader.readLine();
+            String[] size_str = input_line.split("\t");
+            log.info(input_line);
+            n1 =  Long.parseLong(size_str[0]);
+            p =  Long.parseLong(size_str[1]);
+            log.info(size_str);
             reader.close();
         } catch (IOException e) {
+            System.out.println("exception right");
             e.printStackTrace();
+            System.exit(1);
         }
 
         //check matrixes's sizes
-        assert (n.equals(p));
-        assert (n != -1 && m != -1 && o != -1);
+        assert (n == n1);
+        assert (n != -1 && m != -1 && p != -1);
 
-        conf.set("m_size", m.toString());
-        conf.set("n_size", n.toString());
-        conf.set("o_size", o.toString());
+        conf.set("m_size", Long.toString(m));
+        conf.set("n_size", Long.toString(n));
+        conf.set("p_size", Long.toString(p));
 
         Job multBlocks = new Job(conf, "multBlocks");
         multBlocks.setNumReduceTasks(conf.getInt("mapred.reduce.tasks", 1));
@@ -276,14 +298,18 @@ public class mm {
         multBlocks.setInputFormatClass(TextInputFormat.class);
         multBlocks.setOutputFormatClass(TextOutputFormat.class);
 
-        multBlocks.setMapperClass(BlockMapper.class);
-        //multBlocks.setReducerClass(BlockReducer.class);
+        multBlocks.setMapperClass(MultBlockMapper.class);
+        //multBlocks.setReducerClass(MultBlockReducer.class);
 
         multBlocks.setOutputKeyClass(MatrixBlockKey.class);
         multBlocks.setOutputValueClass(MatrixBlockValue.class);
 
-        FileInputFormat.addInputPath(multBlocks, new Path(otherArgs[0]+ "/data") );
-        FileInputFormat.addInputPath(multBlocks, new Path(otherArgs[1]+ "/data"));
+
+
+        String pathLeft = otherArgs[0]+ "/data";
+        String pathRight = otherArgs[1]+ "/data";
+        FileInputFormat.addInputPaths(multBlocks, pathLeft + "," + pathRight);
+      //  FileInputFormat.addInputPath(multBlocks, new Path(otherArgs[1]+ "/data"));
         FileOutputFormat.setOutputPath(multBlocks, new Path("tmp"));
 
 
