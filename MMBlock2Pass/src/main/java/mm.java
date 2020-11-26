823c2d73cb4e18d613f304a2a602629f25f5f655
nt1tov <1nikolay.titov@gmail.com>
2020-11-26 16:12:05 +0300

    minor upd

--- MMBlock2Pass/src/main/java/mm.java
+++ MMBlock2Pass/src/main/java/mm.java
@@ -22,135 +22,140 @@ import org.apache.hadoop.io.ObjectWritable;
 import org.apache.hadoop.io.Writable;
 import java.io.DataInput;
 import java.io.DataOutput;
-
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
 import java.io.IOException;
 
 
-class MatrixBlockKey implements WritableComparable<MatrixBlockKey>{
-    private LongWritable rowBlock;
-    private LongWritable colBlock;
-    private LongWritable dupNumber;
 
-    public MatrixBlockKey(){
-        rowBlock = new LongWritable(0);
-        colBlock = new LongWritable(0);
-        dupNumber = new LongWritable(0);
-    }
-
-    public MatrixBlockKey(long Row, long Column, long dup){
-        rowBlock = new LongWritable(Row);
-        colBlock = new LongWritable(Column);
-        dupNumber = new LongWritable(dup);
-    }
-
-    public long getBlockRow(){
-        return rowBlock.get();
-    }
-
-    public long getBlockCol(){
-        return colBlock.get();
-    }
-
-    public long getDupNumber(){
-        return dupNumber.get();
-    }
-
-    @Override
-    public int compareTo(MatrixBlockKey other) {
-        int compareRow = rowBlock.compareTo(other.rowBlock);
-        int compareCol = colBlock.compareTo(other.colBlock);
-        int compareDupNumber = colBlock.compareTo(other.dupNumber);
-
-        if (compareRow == 0) {
-            if (compareCol == 0) {
-                return compareDupNumber;
-            } else {
-                return compareCol;
-            }
-        } else {
-            return compareRow;
-        }
-    }
-
-    @Override
-    public void write(DataOutput DataOutput) throws IOException{
-        rowBlock.write(DataOutput);
-        colBlock.write(DataOutput);
-        dupNumber.write(DataOutput);
-    }
-
-    @Override
-    public void readFields(DataInput Datainput)  throws IOException{
-        rowBlock.readFields(Datainput);
-        colBlock.readFields(Datainput);
-        dupNumber.readFields(Datainput);
-    }
-
-}
-
-
-class MatrixBlockValue implements Writable{
-    private ObjectWritable matrixTag;
-    private LongWritable colLocal;
-    private LongWritable rowLocal;
-    private DoubleWritable value;
-
-
-    public MatrixBlockValue(){
-        colLocal = new LongWritable(0);
-        rowLocal = new LongWritable(0);
-        value = new DoubleWritable(0.0);
-        matrixTag = new ObjectWritable("");
-
-    }
-
-    public MatrixBlockValue(String tag, long row, long column, double val){
-        matrixTag = new ObjectWritable(tag);
-        rowLocal = new LongWritable(row);
-        colLocal = new LongWritable(column);
-        value = new DoubleWritable(val);
-    }
-
-    public Object getMatrixTag(){
-        return matrixTag.get();
-    }
-
-    public long getRowLocal(){
-        return rowLocal.get();
-    }
-
-    public long getBlockCol(){
-        return colLocal.get();
-    }
-
-    public double getValue(){
-        return value.get();
-    }
-
-    @Override
-    public void write(DataOutput DataOutput) throws IOException{
-        matrixTag.write(DataOutput);
-        rowLocal.write(DataOutput);
-        colLocal.write(DataOutput);
-        value.write(DataOutput);
-    }
-
-    @Override
-    public void readFields(DataInput Datainput)  throws IOException{
-        matrixTag.readFields(Datainput);
-        rowLocal.readFields(Datainput);
-        colLocal.readFields(Datainput);
-        value.readFields(Datainput);
-    }
-
-}
 
 
 public class mm {
+    public static class MatrixBlockKey implements WritableComparable<MatrixBlockKey>{
+        private LongWritable rowBlock;
+        private LongWritable colBlock;
+        private LongWritable groupIdx;
+
+        public MatrixBlockKey(){
+            rowBlock = new LongWritable(0);
+            colBlock = new LongWritable(0);
+            groupIdx = new LongWritable(0);
+        }
+
+        public MatrixBlockKey(long Row, long Column, long dup){
+            rowBlock = new LongWritable(Row);
+            colBlock = new LongWritable(Column);
+            groupIdx = new LongWritable(dup);
+        }
+
+        public long getBlockRow(){
+            return rowBlock.get();
+        }
+
+        public long getBlockCol(){
+            return colBlock.get();
+        }
+
+        public long getGroupIdx(){
+            return groupIdx.get();
+        }
+
+        @Override
+        public int compareTo(MatrixBlockKey other) {
+            int compareRow = rowBlock.compareTo(other.rowBlock);
+            int compareCol = colBlock.compareTo(other.colBlock);
+            int compareGroupIdx= colBlock.compareTo(other.groupIdx);
+
+            if (compareRow == 0) {
+                if (compareCol == 0) {
+                    return compareGroupIdx;
+                } else {
+                    return compareCol;
+                }
+            } else {
+                return compareRow;
+            }
+        }
+
+        @Override
+        public void write(DataOutput DataOutput) throws IOException{
+            rowBlock.write(DataOutput);
+            colBlock.write(DataOutput);
+            groupIdx.write(DataOutput);
+        }
+
+        @Override
+        public void readFields(DataInput Datainput)  throws IOException{
+            rowBlock.readFields(Datainput);
+            colBlock.readFields(Datainput);
+            groupIdx.readFields(Datainput);
+        }
+
+    }
+
+    public static class MatrixBlockValue implements Writable{
+        private ObjectWritable matrixTag;
+        private LongWritable colLocal;
+        private LongWritable rowLocal;
+        private DoubleWritable value;
+
+
+        public MatrixBlockValue(){
+            colLocal = new LongWritable(0);
+            rowLocal = new LongWritable(0);
+            value = new DoubleWritable(0.0);
+            matrixTag = new ObjectWritable("");
+
+        }
+
+        public MatrixBlockValue(String tag, long row, long column, double val){
+            matrixTag = new ObjectWritable(tag);
+            rowLocal = new LongWritable(row);
+            colLocal = new LongWritable(column);
+            value = new DoubleWritable(val);
+        }
+
+        public Object getMatrixTag(){
+            return matrixTag.get();
+        }
+
+        public long getRowLocal(){
+            return rowLocal.get();
+        }
+
+        public long getBlockCol(){
+            return colLocal.get();
+        }
+
+        public double getValue(){
+            return value.get();
+        }
+
+        @Override
+        public void write(DataOutput DataOutput) throws IOException{
+            matrixTag.write(DataOutput);
+            rowLocal.write(DataOutput);
+            colLocal.write(DataOutput);
+            value.write(DataOutput);
+        }
+
+        @Override
+        public void readFields(DataInput Datainput)  throws IOException{
+            matrixTag.readFields(Datainput);
+            rowLocal.readFields(Datainput);
+            colLocal.readFields(Datainput);
+            value.readFields(Datainput);
+        }
+
+    }
+
     public static class BlockMapper extends Mapper<Object, Text, MatrixBlockKey, MatrixBlockValue> {
 
         public static final Log log = LogFactory.getLog(BlockMapper.class);
 
+        @Override
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             Configuration conf = context.getConfiguration();
 
@@ -169,8 +174,9 @@ public class mm {
             long row =  Long.parseLong(input_record[2]);
             double val = Double.parseDouble(input_record[3]);
 
-            String LeftMatrixTag = conf.getStrings("mm.tags", "")[0];
-            String RightMatrixTag = conf.getStrings("mm.tags", "")[1];
+            log.info(conf.get("mm.tags"));
+            String LeftMatrixTag = "A";
+            String RightMatrixTag = "B";
 
             MatrixBlockKey outKey;
             MatrixBlockValue outVal;
@@ -199,15 +205,17 @@ public class mm {
         }
     }
 
+
+
     public static class BlockReducer extends Reducer<Text, Text, Text, Text> {
-        public static final Log log = LogFactory.getLog(BlockReducer.class); // поле класса
-        private Text res_key = new Text();
-        private Text res_values = new Text();
+        public static final Log log = LogFactory.getLog(BlockReducer.class);
 
+        @Override
         public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+            Configuration conf = context.getConfiguration();
+            int BLOCK_SIZE = conf.getInt("mm.groups", 1);
 
 
-            context.write(res_key, res_values);
         }
     }
 
@@ -218,6 +226,7 @@ public class mm {
     public static void main(String[] args) throws Exception {
         Configuration conf = new Configuration();
         String[] otherArgs = new GenericOptionsParser(conf, args).getRemainingArgs();
+
         if (otherArgs.length != 3) {
             System.err.println("Usage: mm <A path> <B path> <C path>");
             System.exit(2);
@@ -260,24 +269,41 @@ public class mm {
         conf.set("n_size", n.toString());
         conf.set("o_size", o.toString());
 
-        Job job = new Job(conf, "mm");
-        job.setNumReduceTasks(conf.getInt("mapred.reduce.tasks", 1));
-        job.setJarByClass(mm.class);
-        job.setMapperClass(BlockMapper.class);
-        job.setReducerClass(BlockReducer.class);
+        Job multBlocks = new Job(conf, "multBlocks");
+        multBlocks.setNumReduceTasks(conf.getInt("mapred.reduce.tasks", 1));
+        multBlocks.setJarByClass(mm.class);
 
-        job.setMapOutputKeyClass(Text.class);
-        job.setMapOutputValueClass(Text.class);
+        multBlocks.setInputFormatClass(TextInputFormat.class);
+        multBlocks.setOutputFormatClass(TextOutputFormat.class);
 
-        job.setOutputKeyClass(Text.class);
-        job.setOutputValueClass(Text.class);
+        multBlocks.setMapperClass(BlockMapper.class);
+        //multBlocks.setReducerClass(BlockReducer.class);
 
-       // job.setInputFormatClass(CSVHeaderFormat.class);
+        multBlocks.setOutputKeyClass(MatrixBlockKey.class);
+        multBlocks.setOutputValueClass(MatrixBlockValue.class);
 
-        FileInputFormat.addInputPath(job, new Path(otherArgs[0]+ "/data") );
-        FileInputFormat.addInputPath(job, new Path(otherArgs[1]+ "/data"));
-        FileOutputFormat.setOutputPath(job, new Path(otherArgs[2] + "/data"));
+        FileInputFormat.addInputPath(multBlocks, new Path(otherArgs[0]+ "/data") );
+        FileInputFormat.addInputPath(multBlocks, new Path(otherArgs[1]+ "/data"));
+        FileOutputFormat.setOutputPath(multBlocks, new Path("tmp"));
 
-        System.exit(job.waitForCompletion(true) ? 0 : 1);
+
+
+//        Job sumBlocks = new Job(conf, "secondStep");
+//        sumBlocks.setJarByClass(mm.class);
+//
+////        sumJob.setMapperClass(SumMapper.class);
+////        sumJob.setReducerClass(SumReducer.class);
+//
+//        sumBlocks.setInputFormatClass(TextInputFormat.class);
+//        sumBlocks.setOutputFormatClass(TextOutputFormat.class);
+//
+//        sumBlocks.setOutputKeyClass(MatrixCoords.class);
+//        sumBlocks.setOutputValueClass(DoubleWritable.class);
+//
+//        FileInputFormat.addInputPath(sumBlocks, new Path("tmp"));
+//        FileOutputFormat.setOutputPath(sumBlocks, new Path(otherArgs[2] + "/data"));
+
+        System.exit(multBlocks.waitForCompletion(true)
+                /*&& sumBlocks.waitForCompletion(true) */ ? 0 : 1);
     }
 }
